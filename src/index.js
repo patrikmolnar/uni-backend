1fce7cd4c8e49e61bb6e1c2e04a84a928f468763
Patrik Molnar <info@patrikmolnar.com>
2019-06-19 13:29:48 +0700

    add mongodb atlas, secure with env

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import express from 'express'
 import mongoose from 'mongoose'
 import { typeDefs } from './typeDefs'
 import { resolvers } from './resolvers'
-
+import dotenv from 'dotenv'
 
 const startServer = async () => {
+	dotenv.config()
 	const app = express()
 
-	const PORT = process.env.PORT || 4000
+	const PORT = process.env.PORT
 
 	const server = new ApolloServer({
 		typeDefs,
@@ -17,7 +18,7 @@ const startServer = async () => {
 	
 	server.applyMiddleware({ app });
 
-	await mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useFindAndModify: false });
+	await mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@${process.env.CLUSTER}/${process.env.DBNAME}`, { useNewUrlParser: true, useFindAndModify: false });
 	
 	app.listen({ port: PORT }, () =>
 		console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
