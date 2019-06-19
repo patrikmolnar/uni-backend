5959dbfc9b3e6cbeb9bb9afab7d3e76130ea2d38
Patrik Molnar <info@patrikmolnar.com>
2019-06-19 17:44:12 +0700

    encrypt password, check for duplicate users

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,4 +1,5 @@
 import { User } from './models/User'
+import bcrypt from 'bcryptjs'
 
 export const resolvers = {
 	Query: {
@@ -7,12 +8,33 @@ export const resolvers = {
 		userByType: (_, args) => User.find({ userType: args.userType }),
 	},
 	Mutation: {
-		createUser: async (_, args) => {
-			const user = new User(args.input);
+		createUser: (_, args) => {
 
-			await user.save();
-			return user;
+			return User.findOne({ email: args.input.email }).then(user => {
+				if (user) {
+					throw new Error('User already exists!')
+				}
+				return bcrypt
+				.hash(args.input.password, 12)
+			})
+			.then(hashedPassword => {
+				const user = new User({
+					email: args.input.email,
+					password: hashedPassword,
+					firstName: args.input.firstName,
+					lastName: args.input.lastName,
+					userType: args.input.userType
+				});
+				return user.save();
+			})
+			.then(result => {
+				return { ...result._doc, password: null, id: result.id };
+			})
+			.catch(err => {
+				throw err;
+			})
 		},
+
 		updateUser: (_, args) =>  User.findOneAndUpdate({ _id: args.id }, args.input),
 		deleteUser: async (_, args) => {
 			const res = await User.deleteOne({ _id: args.id })
