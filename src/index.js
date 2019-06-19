148c5fdce414751948548c7fbe77b983fb865d96
Patrik Molnar <info@patrikmolnar.com>
2019-06-19 12:21:03 +0700

    change port for heroku

--- src/index.js
+++ src/index.js
@@ -17,9 +17,10 @@ const startServer = async () => {
 
 	await mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useFindAndModify: false });
 	
-	app.listen({ port: 4000 }, () =>
+	app.listen({ port: process.env.PORT || 4000 }, () =>
 		console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 	)
+	
 }
 
 startServer();
