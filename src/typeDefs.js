50a291eaee07315e13f6686048579ce058ea62aa
Patrik Molnar <info@patrikmolnar.com>
2019-06-19 17:44:12 +0700

    encrypt password, check for duplicate users

--- src/typeDefs.js
+++ src/typeDefs.js
@@ -3,14 +3,14 @@ import { gql } from 'apollo-server-express'
 export const typeDefs = gql`
 	type Query {
 		users: [User]
-		user(id:ID): User
+		user(id:ID!): User
 		userByType(userType: UserType): [User]
 	}
 
 	type Mutation {
-		createUser(input: UserInput): User!
-		updateUser(id: ID, input: UserInput): User!
-		deleteUser(id: ID): Boolean
+		createUser(input: UserInput): User
+		updateUser(id: ID!, input: UserInput): User
+		deleteUser(id: ID!): Boolean
 	}
 
 	type User {
